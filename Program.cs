47da0f076e4b3c767cc61ebb503dce86905f5f16
Masterzz <61717705+not-masterzz@users.noreply.github.com>
2023-03-18 16:19:33 -0400

    Yes.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace cs_json_parser
+﻿using System;
+
+namespace cs_json_parser
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
         }
     }
 }
